dd60c80055748baca1505af53967fd1aa539b56a
Semen Sereda <semyon3336@gmail.com>
2022-03-29 18:12:21 +1000

    pattern project

---
+++ app/src/main/java/com/example/fitness_feip/ui/login/LoginViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.fitness_feip.ui.login
+
+import androidx.lifecycle.ViewModel
+
+class LoginViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
---
+++ app/src/main/java/com/example/fitness_feip/ui/welcome/WelcomeViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.fitness_feip.ui.welcome
+
+import androidx.lifecycle.ViewModel
+
+class WelcomeViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
